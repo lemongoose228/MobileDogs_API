00c2e384b23dc35fdafabb69a8363e565bdec2eb
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-05-30 16:06:20 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ from fastapi import FastAPI
 import uvicorn
 from database import BaseDBModel, engine
 from users.router import router
-from devices.router import router as router_dev
+from devices.router import router as router1
 
 BaseDBModel.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(router)
-app.include_router(router_dev)
+app.include_router(router1)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
-    
+    uvicorn.run(app, host="0.0.0.0", port=8002)
+
 @app.get("/")
 async def root():
     return {"message": "всё работает"}
