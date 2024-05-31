8f08c48c36d16f9366fe0a6674012927d3043250
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 21:35:35 +0800

    Update test_devices.py

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -1,17 +1,12 @@
-#import src.devices.router as router
-#import src.users.router as router1
 from fastapi.testclient import TestClient
 import random
 import string
-#from src.database import BaseDBModel, engine
-
-#import src.database
 
 from main import app
 
 client = TestClient(app)
 
-async def test_dogs_registration():
+def test_dogs_registration():
     name = ''.join(random.choice(string.ascii_lowercase) for i in range(7))
     collar_id = str(random.randint(0, 200))
 
