ade8c5cd063e9ffcc84202f838eff8d68ca6f95c
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-05-31 21:07:05 +0800

    Update models.py

--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Column
+from sqlalchemy import Integer, String, Column, Boolean
 from database import BaseDBModel
 
 class usersT(BaseDBModel):
@@ -7,13 +7,22 @@ class usersT(BaseDBModel):
     login = Column(String, unique=True, index=True)
     password = Column(String, unique=False, index=False)
     accessToken = Column(String, unique=False, index=False)
-    
+
 class tasksT(BaseDBModel):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_token = Column(String, unique=False, index=True)
     colar_id = Column(Integer, unique=False, index=True)
     text = Column(String, unique=False, index=False)
+    taken = Column(Boolean, default=False)
+
+class responsesT(BaseDBModel):
+    __tablename__ = "responses"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    do_user_id = Column(Integer, unique=False, index=True)
+    task_id = Column(Integer, unique=False, index=True)
+    answer = Column(String, unique=False)
+    readiness = Column(Boolean, default=False)
 
 class subsT(BaseDBModel):
     __tablename__ = "subscriptions"
