1e2c9079ec1e21985b85c07107cbbba2245313b3
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 21:11:59 +0800

    Update test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,29 +1,32 @@
+#import src.devices.router as router
+#import src.users.router as router1
+from fastapi.testclient import TestClient
 import random
 import string
 #from src.database import BaseDBModel, engine
 
 #import src.database
-from fastapi.testclient import TestClient
+
 from main import app
 
 client = TestClient(app)
 
-async def test_users_registration():
+def test_dogs_registration():
     name = ''.join(random.choice(string.ascii_lowercase) for i in range(7))
-    password = ''.join(random.choice(string.ascii_lowercase) for i in range(9))
+    collar_id = str(random.randint(0, 200))
 
-    response = client.post("/user/registr", json={
-            "login": name,
-            "password": password,
+    response = client.post("/dogs/register", json={
+            "name": name,
+            "collar_id": collar_id,
     })
 
     while (response.status_code!=200):
-        name = ''.join(random.choice(string.ascii_lowercase) for i in range(7))
-        password = ''.join(random.choice(string.ascii_lowercase) for i in range(9))
+        name = rand_string = ''.join(random.choice(string.ascii_lowercase) for i in range(7))
+        collar_id = random.randint(0, 200)
 
-        response = client.post("/user/registr", json={
-            "login": name,
-            "password": password,
+        response = client.post("/dogs/register", json={
+            "name": name,
+            "collar_id": collar_id,
         })
 
-    return response.json()["accessToken"]
+    return response.json()["collar_token"]
