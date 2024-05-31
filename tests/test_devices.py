826eaf89f75f2431f50c3c6a1766cc8673540bf6
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 18:22:40 +0800

    Update test_devices.py

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -1,1 +1,35 @@
+import pytest
+#import src.devices.router as router
+#import src.users.router as router1
+from fastapi.testclient import TestClient
+import random
+from src.database import BaseDBModel, engine
 
+from src.main import app
+
+client = TestClient(app)
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
+
+async def test_dogs_registration():
+    name = generate_random_string(7)
+    collar_id = random.randint(0, 200)
+
+    response = client.post("/dogs/register", json={
+            "name": name,
+            "collar_id": collar_id,
+    })
+
+    while (response.status_code!=200):
+        name = generate_random_string(7)
+        collar_id = random.randint(0, 200)
+
+        response = client.post("/dogs/register", json={
+            "name": name,
+            "collar_id": collar_id,
+        })
+
+    return response.json()["collar_token"]
