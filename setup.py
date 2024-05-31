21c40248e324bd62f90598930cd576fe3dcbf5d5
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 22:42:57 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='MobileDogs_API',
+   version='1.0',
+   description='For vector operations',
+   license='MIT',
+   author='Oksana Galiulina, Konova Ekaterina',
+   author_email='oksana17.06@bk.ru',
+   url='https://github.com/mgwoorl/my_project2',
+   packages=find_packages(exclude=['test']), 
+   install_requires=['numpy'],
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
+)
