c8240e2bf3fbb30d476f1e7d0b627ad6342f0580
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-06-01 00:07:17 +0800

    Add files via upload

---
+++ scenarios/user_unsubscribe.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "http://localhost:8002/user/unsubscribe"
+data = {
+    "user_login": "user30301",
+    "collar_id": "12",
+    "accessToken": "870cb511"
+}
+
+response = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(data))
+print(response.status_code, response.json())
