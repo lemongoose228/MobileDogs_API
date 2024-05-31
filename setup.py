ac4021af3d861379990c2c8fcbf8df0d85558245
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 22:44:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
    name='MobileDogs_API',
    version='1.0',
-   description='For vector operations',
+   description='For tracking homeless dogs',
    license='MIT',
    author='Oksana Galiulina, Konova Ekaterina',
    author_email='oksana17.06@bk.ru',
-   url='https://github.com/mgwoorl/my_project2',
+   url='https://github.com/lemongoose228/MobileDogs_API',
    packages=find_packages(exclude=['test']), 
    install_requires=['numpy'],
    extras_require={
