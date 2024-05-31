bc6cad95c05d2491849f335da5b19dc39fc37581
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-05-31 21:09:28 +0800

    Update exceptions.py

--- src/devices/exceptions.py
+++ src/devices/exceptions.py
@@ -3,3 +3,7 @@ from fastapi import HTTPException, status
 class BusyCollar(HTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Этот ошейник занят")
+
+class NotExistCollar(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Собака с таким ошейником не зарегистрирована")
