481c018a5867bd2bbb31c14be7910b35beffa16e
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 23:01:45 +0800

    Update user_reg.py

--- scenarios/user_reg.py
+++ scenarios/user_reg.py
@@ -1,1 +1,11 @@
+import requests
+import json
 
+url = "http://localhost:8002/user/registr"
+data = {
+    "login": "user30301",
+    "password": "superpassword"
+}
+
+response = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(data))
+print(response.status_code, response.json())
